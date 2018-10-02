1032cf87957a0ccf20d788db86f5a65bf765bcc7
satorbs <mitsui.satoshi@runners.co.jp>
2018-10-02 10:09:45 +0900

    add Particles

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ module.exports = exports = {
     Threebox: require('./src/Threebox'),
     THREE: require('./src/three64.js'),
     OBJLoader: require('./src/Loaders/OBJLoader'),
-    MTLLoader: require('./src/Loaders/MTLLoader')
+    MTLLoader: require('./src/Loaders/MTLLoader'),
+    ShaderParticleEmitter: require('./src/Particles/ShaderParticleEmitter'),
+    ShaderParticleGroup: require('./src/Particles/ShaderParticleGroup')
 }
